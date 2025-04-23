40fde88139ae4a6ca28f6048e1a0f622e65291b6
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2025-04-23 16:55:09 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.scss';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { Layout } from './shared/Layout';
+import { HomePage } from './modules/HomePage';
+import { Phones } from './modules/Phones';
+import { Tablets } from './modules/Tablets/Tablets';
+import { Accessories } from './modules/Accessories';
+import { ProductInfo } from './modules/ProductInfo/ProductInfo';
+import { Cart } from './modules/Cart';
+import { Favourites } from './modules/Favourites/Favourites';
+import './i18n/i18n';
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<HomePage />} />
+
+        <Route path="/phones" element={<Phones />} />
+
+        <Route path="/tablets" element={<Tablets />} />
+
+        <Route path="/accessories" element={<Accessories />} />
+
+        <Route path="/case" element={<Cart />} />
+
+        <Route path="/favourites" element={<Favourites />} />
+
+        <Route path="/:category/:modelName" element={<ProductInfo />} />
+
+        <Route
+          path="*"
+          element={<img src="/img/page-not-found.png" alt="Page not found" />}
+        />
+      </Routes>
+    </Layout>
   );
 };
